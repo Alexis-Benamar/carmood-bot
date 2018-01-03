9d694df70fb07683017402386b890c4c60b20907
Alexis-Benamar <alexis.benamar@viacesi.fr>
2018-01-03 14:57:00 +0000

    added request threading

--- bot.js
+++ bot.js
@@ -6,12 +6,19 @@ if (process.env.NODE_ENV !== 'production') {
     console.log("// PROD //\n");
 }
 
-const Twit = require("twit");
 const config = {
     consumer_key:         process.env.consumer_key,
     consumer_secret:      process.env.consumer_secret,
     access_token:         process.env.access_token,
     access_token_secret:  process.env.access_token_secret
 };
-const fs = require('fs');     // File system on
-const T = new Twit(config);   // Initializing twitter bot
+const fs = require('fs');
+const Twit = require("twit");
+const T = new Twit(config);
+let stream = T.stream('user');
+
+stream.on('tweet', setTimeout(rdm_car(eventMsg), 10000));
+
+function rdm_car(eventMsg) {
+    console.log('test timeout ' + eventMsg.text);
+}
